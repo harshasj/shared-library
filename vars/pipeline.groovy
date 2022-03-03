8d5d0af9834b76977a467d3a49902a52be36c0a0
harshasj <36756863+harshasj@users.noreply.github.com>
2022-03-03 20:15:56 +0530

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -2,16 +2,18 @@ import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
 def call (body){
 
-def config = [:]
-body.resolveStrategy = Closure.DElEGATE_FIRST
-body.delegate = config
-body ()
-pipeline{
-    agent any 
-    stage("library"){
-        sh """
-        echo "the library loaded "
-        """
+    def config = [:]
+    body.resolveStrategy = Closure.DElEGATE_FIRST
+    body.delegate = config
+    body ()
+    pipeline{
+        agent any 
+        stages {
+            stage("library"){
+                sh """
+                echo "the library loaded "
+                """
+            }
+        }
     }
-}
 }
